bf24f2cb6080085c3637d38f4d60ceddbd40eaed
phipno <phipno@gmail.com>
2023-12-15 17:36:50 +0100

    wordpress could work now

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
-
-if [ -f ./wordpress/wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
-	#Download wordpress
-	wget https://wordpress.org/latest.tar.gz
-	tar -xzvf latest.tar.gz
-	rm -rf latest.tar.gz
-
-	#Update configuration file
-	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
-	mv ./www.conf /etc/php/7.3/fpm/pool.d/
-
-	#Inport env variables in the config file
-	cd /var/www/html/wordpress
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	mv wp-config-sample.php wp-config.php
-fi
-
-exec "$@"
+# set listening port for php fpm
+sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php${PHP_VERSION}/php-fpm.d/www.conf
+# waiting for the database to be ready - could be done in loop too
+# sleep 30
+wp config create --allow-root \
+					--dbname=${DB_NAME} \
+					--dbuser=${DB_USER} \
+					--dbpass=${DB_PASS} \
+					--url=${WP_URL} \
+					--force
+wp core install --allow-root \
+				--title=${WP_TITLE} \
+				--url=${WP_URL} \
+				--admin_user=${WP_ADMIN_USER} \
+				--admin_password=${WP_ADMIN_PASS} \
+				--admin_email=${WP_ADMIN_EMAIL}
+wp user create ${WP_USER} ${WP_USER_EMAIL} \
+			--allow-root \
+			--user_pass=${WP_USER_PASS}
+wp option update home ${WP_URL}
+wp option update siteurl ${WP_URL}
+php-fpm${PHP_VERSION} -f
